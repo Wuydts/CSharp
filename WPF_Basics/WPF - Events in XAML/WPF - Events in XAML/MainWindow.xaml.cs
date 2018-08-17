8425bac0170ad462eb8e49f257d095166a765878
Wuydts <42002906+Wuydts@users.noreply.github.com>
2018-08-17 12:24:37 -0500

    Add files via upload

---
+++ WPF_Basics/WPF - Events in XAML/WPF - Events in XAML/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF___Events_in_XAML
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Button was clicked - Direct Event");
+        }
+
+        private void Button_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show("Mouse Button Up was released - Bubbling Event");
+        }
+
+        private void Button_PreviewMouseUp(object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show("Mouse button up was released - Tunneling event");
+        }
+
+        private void Button_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show("Left Mouse button went down was not released - tunneling event");
+        }
+
+        private void Button_PreviewMouseRightButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            MessageBox.Show("Right Mouse button went Up was not released - tunneling event");
+        }
+    }
+}
+
+/*
+ *  Routed Event - WPF Mechanism to handle events
+ *  
+ *  
+ * Direct Routed Events - Handled by the item itself (click)
+ * 
+ * Bubbling Event - Happens when the event is not handled by the element (ie a text box)
+ * and the event "Bubbles" its way up the UI containers which hold it. 
+ * 
+ * 
+ * Tunneling Event - Opposite of bubbling. So instead of going up the visual tree, it travels down the visual tree. 
+ * 
+ * 
+ * 
+ */
