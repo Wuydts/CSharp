1184d650c4bff57622ef80c3532af2bfdcfd1d99
Wuydts <42002906+Wuydts@users.noreply.github.com>
2018-08-17 12:20:09 -0500

    Add files via upload

---
+++ Coding_Basics/Nullables/Nullables/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Nullables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ? num1 = null;
+            int ? num2 = 1337;
+
+            double ? num3 = new Double?();
+
+            bool ? boolval = new bool?();
+
+            Console.WriteLine("Our nullable numbers are: {0}, {1}, {2} ", num1, num2, num3);
+            Console.WriteLine("The nullable boolean value is {0} ", boolval);
+        }
+    }
+}
---
+++ Coding_Basics/RegularExpressions/RegularExpressions/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace RegularExpressions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"\d";
+           
+
+            string text = "Hello! My number is 12314";
+            regExp(pattern, text);
+
+            pattern = @"\d{5}";
+            regExp(pattern, text);
+
+            pattern = @"Hello";
+            regExp(pattern, text);
+
+
+        }
+
+        private static void regExp(string pattern, string text)
+        {
+            Regex regex = new Regex(pattern);
+            MatchCollection match = regex.Matches(text);
+            Console.WriteLine("{0} hits found: \n {1} ", match.Count, text);
+
+            foreach (Match hit in match)
+            {
+                GroupCollection group = hit.Groups;
+                Console.WriteLine("{0} found at {1}", group[0].Value, group[0].Index);
+            }
+        }
+    }
+}
