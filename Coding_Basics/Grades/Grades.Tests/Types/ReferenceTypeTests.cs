cc06063ff90f2dc9202ac2e50010ae8dea5c8edf
Wuydts <42002906+Wuydts@users.noreply.github.com>
2018-08-17 12:22:18 -0500

    Add files via upload

---
+++ Coding_Basics/Grades/Grades.Tests/Types/ReferenceTypeTests.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Grades.Tests.Types
+{
+    // control k + x for code snippet
+    // type test and press tab to sort through test snippets
+    [TestClass]
+    public class TypeTests
+    {
+        [TestMethod]
+        public void UsingArrays()
+        {
+            float[] grades; 
+            grades = new float[3];
+
+            AddGrades(grades);
+
+            Assert.AreEqual(89.1f, grades[1]);
+        }
+
+        private void AddGrades(float[] grades)
+        {
+            grades[1] = 89.1f;
+        }
+
+
+        [TestMethod]
+        public void UppercaseString()
+        {
+            string name = "christopher";
+            name =  name.ToUpper();
+
+            Assert.AreEqual("CHRISTOPHER", name);
+
+        }
+        [TestMethod]
+        public void addDaysToDateTime()
+        {
+            DateTime date = new DateTime(2018, 5, 25);
+            date = date.AddDays(1);
+            
+
+            Assert.AreEqual(26, date.Day);
+        }
+
+
+
+        [TestMethod]
+        public void ValueTypesPassByValue()
+        {
+            int x = 46;
+            Increamentnumber(ref x); // coppied into number. but we never return it back to this... which is why its still correct
+            // thus its obviously pass by value.
+            Assert.AreEqual(47, x);
+
+        }
+
+        private void Increamentnumber(ref int number)
+        {
+            number += 1;
+        }
+        [TestMethod]
+        public void ReferenceTypesPassByValue()
+        {
+            GradeBook book1 = new GradeBook();
+            GradeBook book2 = book1;
+
+            GiveBookAName(book2);
+            Assert.AreEqual("A gradebook", book1.Name);
+        }
+
+        private void GiveBookAName(GradeBook book)
+        {
+            book.Name = "A gradebook";
+        }
+        
+        [TestMethod]
+        public void StringCOmparisons()
+        {
+            string name1 = "Scott";
+            string name2 = "scott";
+
+            bool result = string.Equals(name1, name2, StringComparison.InvariantCultureIgnoreCase);
+            Assert.IsTrue(result);
+        }
+
+
+        [TestMethod]
+        public void IntVariablesHoldAValue()
+        {
+            int x1 = 100;
+            int x2 = x1;
+
+            x1 = 4;
+            Assert.AreNotEqual(x1, x2);
+        }
+
+        [TestMethod]
+        public void GradeBookVariablesHoldAReference()
+        {
+            GradeBook g1 = new GradeBook();
+            GradeBook g2 = g1;
+
+            g1.Name = "Chris's grade book";
+            Assert.AreEqual(g1.Name, g2.Name);
+            
+        }
+
+
+        
+    }
+}
---
+++ Coding_Basics/Grades/Grades/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            IGradeTracker book = CreateGradeBook();
+
+
+
+         //   GetBookName(book);
+            CreateGrades(book);
+            SaveGrades(book);
+            WriteResults(book);
+
+        }
+
+        private static IGradeTracker CreateGradeBook()
+        {
+            return new ThrowAwayGradeBook();
+        }
+
+        private static void WriteResults(IGradeTracker book)
+        {
+            GradeStatistics stats = book.ComputeStatistics();
+
+            foreach (float grade in book)
+            {
+                Console.WriteLine(grade);
+            }
+
+            writeResult("Highest: ", (int)stats.HighestGrade);
+            writeResult("Lowest : ", (double)stats.LowestGrade);
+            writeResult("Average: ", stats.AverageGrade);
+            writeResult(stats.Description, stats.LetterGrade);
+        }
+
+        private static void SaveGrades(IGradeTracker book)
+        {
+            using (StreamWriter outputFile = File.CreateText("grades.txt"))
+            {
+                book.WriteGrades(outputFile);
+            }
+        }
+
+        private static void CreateGrades(IGradeTracker book)
+        {
+            book.AddGrade(91);
+            book.AddGrade(89.5f);
+            book.AddGrade(75);
+        }
+
+        private static void GetBookName(IGradeTracker book)
+        {
+            try
+            {
+                Console.WriteLine("Enter a book name");
+                book.Name = Console.ReadLine();
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+
+            }
+            catch (NullReferenceException ex)
+            {
+                Console.WriteLine("Null Reference. Something went wrong! :" + ex.Message);
+            }
+        }
+
+        static void writeResult(string description, string result)
+        {
+            Console.WriteLine(description + ": " + result);
+        }
+
+        static void OnNameChanged(object sender, NameChangedEventArgs args)
+        {
+            Console.WriteLine($"Grade book changing name from {args.ExistingName} to {args.NewName}");
+        }
+       
+        static void writeResult(string description, int result)
+        {
+            Console.WriteLine(description + ": " + result);
+        }
+
+        static void writeResult(string description, float result)
+        {
+            Console.WriteLine($"{description}: {result:F2} ");
+        }
+        static void writeResult(string description, double result)
+        {
+            Console.WriteLine("{0}: {1:F2} ", description, result);
+        }
+    }
+}
+/* Commented out to work on flow control
+
+            book.NameChanged += OnNameChanged;
+            book.Name = "Chris's grade book";
+            book.Name = null;
+            book.Name = String.Empty;
+            book.Name = "Christophers grade book";
+
+            book.AddGrade(91);
+            book.AddGrade(89.5f);
+            book.AddGrade(75);
+
+
+            GradeStatistics stats = book.ComputeStatistics();
+            Console.WriteLine(book.Name);
+            writeResult("Highest: ", (int)stats.HighestGrade);
+            writeResult("Lowest : ", (double)stats.LowestGrade);
+            writeResult("Average: ", stats.AverageGrade);
+            book.Name = "Chris Wuydts' grade book";
+
+    */
